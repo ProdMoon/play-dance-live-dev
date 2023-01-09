f25a6e53f9f820b58f0dd4ebd03464c3e988139e
Kim Dael <daelkdev@gmail.com>
2023-01-09 20:34:58 +0900

    :construction: RoomRepository

--- backend/src/main/java/com/example/manmu/entity/Song.java
+++ backend/src/main/java/com/example/manmu/entity/Song.java
@@ -1,7 +1,16 @@
 package com.example.manmu.entity;
 
+import javax.persistence.*;
+
+@Entity
 public class Song {
+    @Id
     private Integer songId;
+    @Column
     private String songName;
+    @Column
     private String singer;
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
 }
