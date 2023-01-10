2bd3d6502c05e2913b5c348f26f872787ae6adc1
Kim Dael <daelkdev@gmail.com>
2023-01-10 23:48:30 +0900

    :construction: Chatmessage

--- backend/src/main/java/com/example/manmu/ChatMessage.java
+++ backend/src/main/java/com/example/manmu/ChatMessage.java
@@ -1,9 +1,15 @@
 package com.example.manmu;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChatMessage {
     private MessageType type;
     private String content;
     private String sender;
+    private String roomId;
 
     public enum MessageType {
         CHAT,
@@ -11,27 +17,4 @@ public class ChatMessage {
         LEAVE
     }
 
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
 }
