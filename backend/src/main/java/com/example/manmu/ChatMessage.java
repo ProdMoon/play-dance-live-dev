91e9169bd97fd3be85911936e15953247ef79019
skqls <kitchin105@gmail.com>
2023-01-11 20:05:51 +0900

    채팅방 이름 수정

--- backend/src/main/java/com/example/manmu/ChatMessage.java
+++ backend/src/main/java/com/example/manmu/ChatMessage.java
@@ -1,5 +1,9 @@
 package com.example.manmu;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class ChatMessage {
     private MessageType type;
     private String content;
@@ -11,27 +15,4 @@ public class ChatMessage {
         LEAVE
     }
 
-    public MessageType getType() {
-        return type;
-    }
-
-    public void setType(MessageType type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
 }
