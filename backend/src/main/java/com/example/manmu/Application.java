67d888307a862c716d0a09107fcd25685c469f34
skqls <kitchin105@gmail.com>
2023-01-24 14:24:10 +0900

    레디스 세션 및 캐시

--- backend/src/main/java/com/example/manmu/Application.java
+++ backend/src/main/java/com/example/manmu/Application.java
@@ -2,8 +2,10 @@ package com.example.manmu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
---
+++ backend/src/main/java/com/example/manmu/config/CacheKey.java
@@ -0,0 +1,6 @@
+package com.example.manmu.config;
+
+public class CacheKey {
+    public static final int DEFAULT_EXPIRE_SEC = 300; // 5 minutes
+
+}
