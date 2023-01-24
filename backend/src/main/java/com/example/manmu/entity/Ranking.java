eb9d1e8f705a074911540b2aa90ad1fa73072911
skqls <kitchin105@gmail.com>
2023-01-24 22:25:33 +0900

    ranking 관련 수정

--- backend/src/main/java/com/example/manmu/entity/Ranking.java
+++ backend/src/main/java/com/example/manmu/entity/Ranking.java
@@ -1,6 +1,8 @@
 package com.example.manmu.entity;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
 import javax.persistence.*;
 
 @Getter
@@ -18,10 +20,18 @@ public class Ranking {
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
 
+    @ColumnDefault("0")
+    private Integer currentWinNums;
+
+    @ColumnDefault("0")
+    private Integer bestWinNums;
+
     @Builder
-    public Ranking(Long id, User user) {
-        this.id = id;
+
+    public Ranking(User user, Integer currentWinNums, Integer bestWinNums) {
         this.user = user;
+        this.currentWinNums = currentWinNums;
+        this.bestWinNums = bestWinNums;
     }
 }
 
